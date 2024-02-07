cbbe2360bfcece61fb5a13dd6d71e2ecb216c394
Consuelo Beckett <consuelo.becktt@gmail.com>
2024-02-07 03:41:14 -0300

    correccion

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,22 +1,22 @@
-import { usersService } from "../services/UserService.js";
-import { createHash, passwordValidation } from "./utils.js";
+import UserService  from "../services/UserService.js";
+import { hashPass, validPass } from "../utils.js";
 import jwt from 'jsonwebtoken';
-import UserDTO from '../dto/User.dto.js';
+import UserDTO from '../dao/DTOs/user.dto.js';
 
 const register = async (req, res) => {
     try {
         const { first_name, last_name, email, password } = req.body;
         if (!first_name || !last_name || !email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-        const exists = await usersService.getUserByEmail(email);
+        const exists = await UserService.getUserByEmail(email);
         if (exists) return res.status(400).send({ status: "error", error: "User already exists" });
-        const hashedPassword = await createHash(password);
+        const hashedPassword = await hashPass(password);
         const user = {
             first_name,
             last_name,
             email,
             password: hashedPassword
         }
-        let result = await usersService.create(user);
+        let result = await UserService.create(user);
         console.log(result);
         res.send({ status: "success", payload: result._id });
     } catch (error) {
@@ -27,9 +27,9 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-    const user = await usersService.getUserByEmail(email);
+    const user = await UserService.getUserByEmail(email);
     if(!user) return res.status(404).send({status:"error",error:"User doesn't exist"});
-    const isValidPassword = await passwordValidation(user,password);
+    const isValidPassword = await validPass(user,password);
     if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
     const userDto = UserDTO.getUserTokenFrom(user);
     const token = jwt.sign(userDto,'tokenSecretJWT',{expiresIn:"1h"});
@@ -46,9 +46,9 @@ const current = async(req,res) =>{
 const unprotectedLogin  = async(req,res) =>{
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-    const user = await usersService.getUserByEmail(email);
+    const user = await UserService.getUserByEmail(email);
     if(!user) return res.status(404).send({status:"error",error:"User doesn't exist"});
-    const isValidPassword = await passwordValidation(user,password);
+    const isValidPassword = await validPass(user,password);
     if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
     const token = jwt.sign(user,'tokenSecretJWT',{expiresIn:"1h"});
     res.cookie('unprotectedCookie',token,{maxAge:3600000}).send({status:"success",message:"Unprotected Logged in"})
